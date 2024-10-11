940236546be55aeb5d2fba272858fed4662984c8
twk <twk97@hotmail.com>
2024-10-11 22:08:39 +0800

    check headers

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { chromium } from "playwright";
+import fp from "node:fs/promises";
 
 const upbit_home = "https://upbit.com/home";
 
 // ?os=web&page=1&per_page=20&category=all
-const api_endpoint = "https://api-manager.upbit.com/api/v1/announcements?";
+// const api_endpoint = "https://api-manager.upbit.com/api/v1/announcements?";
+
+const api_endpoint =
+    "https://api-manager.upbit.com/api/v1/announcements?os=web&page=1&per_page=20&category=all";
 
 (async () => {
     const browser = await chromium.launch({
-        headless: false,
+        headless: true,
         devtools: false,
     });
 
@@ -29,41 +33,52 @@ const api_endpoint = "https://api-manager.upbit.com/api/v1/announcements?";
             return;
         }
 
-        if (url.includes(api_endpoint)) {
+        if (url === api_endpoint) {
             const allHeaders = await r.allHeaders();
+            const headers = r.headers();
+            const headersArray = await r.headersArray();
 
-            const res = await r.response();
-
-            if (res) {
-                const resAllHeaders = await res.allHeaders();
-                const resStatus = res.status();
-                const resStatusText = res.statusText();
-                const resJsonData = await res.json();
-
-                console.log("----- request -----");
-                console.log(`request url: ${url}`);
-                console.log(allHeaders);
-                console.log("----- response -----");
-                console.log(resAllHeaders);
-                console.log(resStatus);
-                console.log(resStatusText);
-
-                if ("data" in resJsonData) {
-                    if ("notices" in resJsonData["data"]) {
-                        for (let i = 0; i < resJsonData["data"]["notices"].length; i++) {
-                            if (i > 3) {
-                                break;
-                            }
-                            console.log(resJsonData["data"]["notices"][i]);
-                            console.log(resJsonData["data"]["notices"][i]);
-                            console.log(resJsonData["data"]["notices"][i]);
-                        }
-                    }
-                }
-            } else {
-                console.log("response unsuccessful");
-            }
+            console.log(allHeaders);
+            console.log(headers);
+            headersArray.forEach((h) => {
+                console.log(h);
+            });
+            // await fp.writeFile("./headers.txt", JSON.stringify(headers), { encoding: "utf8" });
         }
+
+        // if (url.includes(api_endpoint)) {
+        //     const allHeaders = await r.allHeaders();
+
+        //     const res = await r.response();
+
+        //     if (res) {
+        //         const resAllHeaders = await res.allHeaders();
+        //         const resStatus = res.status();
+        //         const resStatusText = res.statusText();
+        //         const resJsonData = await res.json();
+
+        //         console.log("----- request -----");
+        //         console.log(`request url: ${url}`);
+        //         console.log(allHeaders);
+        //         console.log("----- response -----");
+        //         console.log(resAllHeaders);
+        //         console.log(resStatus);
+        //         console.log(resStatusText);
+
+        //         if ("data" in resJsonData) {
+        //             if ("notices" in resJsonData["data"]) {
+        //                 for (let i = 0; i < resJsonData["data"]["notices"].length; i++) {
+        //                     if (i >= 2) {
+        //                         break;
+        //                     }
+        //                     console.log(resJsonData["data"]["notices"][i]);
+        //                 }
+        //             }
+        //         }
+        //     } else {
+        //         console.log("response unsuccessful");
+        //     }
+        // }
     });
 
     /* from upbit home -> go to upbit notice page */
@@ -78,5 +93,5 @@ const api_endpoint = "https://api-manager.upbit.com/api/v1/announcements?";
     setTimeout(async () => {
         await context.close();
         await browser.close();
-    }, 6000);
+    }, 13000);
 })();
